92931efcdf3945b21b8c611cddafec92d058b9e2
Beren Ates <berenatess9918@gmail.com>
2023-11-27 23:37:51 +0300

    Sprint11-Day1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import { data } from "./data";
 
@@ -7,26 +7,51 @@ import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
+
 function App() {
   const [products, setProducts] = useState(data);
   const [cart, setCart] = useState([]);
 
   const addItem = (item) => {
-    // verilen itemi sepete ekleyin
+    if (cart.find((cartItem) => cartItem.id === cart.id)) return;
+
+    const newCart = [...cart, item];
+    setCart(newCart);
+    localStorage.setItem("cart", JSON.stringify(newCart));
   };
+
+  const removeItem = (id) => {
+    const newCart = cart.filter((cartItem) => cartItem.id !== id);
+    setCart(newCart);
+    localStorage.setItem("cart", JSON.stringify(newCart));
+  };
+
+  useEffect(() => {
+    const cartUpdate = JSON.parse(localStorage.getItem("cart"));
+    setCart(cartUpdate);
+  }, []);
 
   return (
     <div className="App">
-      <Navigation cart={cart} />
+      <CartContext.Provider value={{ cart }}>
+        <Navigation />{" "}
+      </CartContext.Provider>
 
       {/* Routelar */}
       <main className="content">
         <Route exact path="/">
-          <Products products={products} addItem={addItem} />
+          <ProductContext.Provider value={{ products, addItem }}>
+            {" "}
+            <Products />{" "}
+          </ProductContext.Provider>
         </Route>
 
         <Route path="/cart">
-          <ShoppingCart cart={cart} />
+          <CartContext.Provider value={{ cart, removeItem }}>
+            <ShoppingCart />
+          </CartContext.Provider>
         </Route>
       </main>
     </div>
